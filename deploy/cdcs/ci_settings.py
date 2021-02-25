d05210c656702c923d366984febb6a0f921a0072
Philippe Dessauw <philippe.dessauw@nist.gov>
2021-02-25 09:51:02 -0500

    feat: remove CAPTCHA and Defender lock in pre-production

--- deploy/cdcs/ci_settings.py
+++ deploy/cdcs/ci_settings.py
@@ -2,6 +2,11 @@
 """
 from .settings import *
 
+# Override Defender and Django Simlpe Captcha settings to allow for automatic
+# regression testing.
+DEFENDER_LOGIN_FAILURE_LIMIT = 1000
+CAPTCHA_TEST_MODE = True
+
 SSL_CERTIFICATES_DIR = "certs"
 MONITORING_SERVER_URI = os.environ["MONITORING_SERVER_URI"] if "MONITORING_SERVER_URI" in os.environ else None
 if MONITORING_SERVER_URI:
