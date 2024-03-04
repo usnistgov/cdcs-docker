46249a23b164ef0a9b150ea5c279f2d13f538629
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-03-04 15:33:20 -0500

    feat: custom cdcs images

--- deploy/cdcs/ci_settings.py
+++ deploy/cdcs/ci_settings.py
@@ -2,7 +2,7 @@
 """
 from .settings import *
 
-# Override Defender and Django Simlpe Captcha settings to allow for automatic
+# Override Defender and Django Simple Captcha settings to allow for automatic
 # regression testing.
 DEFENDER_LOGIN_FAILURE_LIMIT = 1000
 CAPTCHA_TEST_MODE = True
