95d21cdd4fddd4b50b5fd44caa8bd072735de6d8
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-07-22 10:20:25 -0400

    fix: wait psql replica (#41)

--- build/cdcs/scripts/wait-psql.sh
+++ build/cdcs/scripts/wait-psql.sh
@@ -6,3 +6,11 @@ until PGPASSWORD=$POSTGRES_PASS psql -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U 
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
+
+if [ ! -z $POSTGRES_REPLICA_HOST ]; then
+  echo "********* Wait for Postgres read replica... *********"
+  until PGPASSWORD=$POSTGRES_REPLICA_PASS psql -h "$POSTGRES_REPLICA_HOST" -p "$POSTGRES_REPLICA_PORT" -U "$POSTGRES_REPLICA_USER" -d "$POSTGRES_REPLICA_DB" -c '\q'; do
+    >&2 echo "Postgres read replica is unavailable - sleeping"
+    sleep 1
+  done
+fi
