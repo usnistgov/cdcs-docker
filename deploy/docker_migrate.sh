71a0cc726a75379c19257cc23ad644a6cc91f2e5
Guillaume-sousa <guillaume.sousa@nist.gov>
2024-10-25 11:27:04 -0400

    feat: set COMPOSE_PROJECT_NAME for multiple deployments

--- deploy/docker_migrate.sh
+++ deploy/docker_migrate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source .env
-docker exec $PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --dry-run --no-input
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --dry-run --no-input
 read -p "Press enter to apply changes:"
-docker exec $PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --no-input
-docker exec $PROJECT_NAME"_cdcs" python ./manage.py migrate --plan
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --no-input
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate --plan
 read -p "Press enter to apply changes:"
-docker exec $PROJECT_NAME"_cdcs" python ./manage.py migrate
+docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate
