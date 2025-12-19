1bd8d779a6b745e499798785e4d636bc1d7e3345
Guillaume-sousa <guillaume.sousa@nist.gov>
2025-12-19 10:59:35 -0500

    feat: add podman support

--- deploy/docker_migrate.sh
+++ deploy/docker_migrate.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 source .env
-docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --dry-run --no-input
+CONTAINER_ENGINE="docker"
+if [[ " $@ " =~ " --podman " ]]; then
+    CONTAINER_ENGINE="podman"
+fi
+set -- "${@/--podman/}"
+
+$CONTAINER_ENGINE exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --dry-run --no-input
 read -p "Press enter to apply changes:"
-docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --no-input
-docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate --plan
+$CONTAINER_ENGINE exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py collectstatic --clear --no-input
+$CONTAINER_ENGINE exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate --plan
 read -p "Press enter to apply changes:"
-docker exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate
+$CONTAINER_ENGINE exec $COMPOSE_PROJECT_NAME"_cdcs" python ./manage.py migrate
